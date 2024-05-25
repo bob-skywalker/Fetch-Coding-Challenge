3c5b2b9b24495266b6c31b254ab73da40ad2bdb9
bo <q463250938@gmail.com>
2024-05-25 02:10:07 -0700

    Initial Commit

---
+++ Fetch-Coding-Challenge/Fetch_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Coding_ChallengeApp.swift
+//  Fetch-Coding-Challenge
+//
+//  Created by Bo Zhong on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
