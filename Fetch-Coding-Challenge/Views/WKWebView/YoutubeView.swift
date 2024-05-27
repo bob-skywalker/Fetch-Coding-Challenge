88094df3f06760d90d24211aab2d467326fe1536
bo <q463250938@gmail.com>
2024-05-27 13:56:54 -0700

    Ui for dessertDetailView completed Added animation for transition between light/dark mode Implemented Youtube WebKit view to surf the youtube link in a sheet view Added @Binding for KFImage and colorScheme for one way data flow

---
+++ Fetch-Coding-Challenge/Views/WKWebView/YoutubeView.swift
@@ -0,0 +1,19 @@
+//
+//  WKWebView.swift
+//  Fetch-Coding-Challenge
+//
+//  Created by Bo Zhong on 5/27/24.
+//
+
+import SwiftUI
+import WebKit
+
+struct YoutubeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YoutubeView()
+}
