729de6319566a10091c3dc01d596d4a40a7d490e
bo <q463250938@gmail.com>
2024-05-26 20:27:38 -0700

    Dessert detailView created

---
+++ Fetch-Coding-Challenge/Views/DessertDetailView/DessertDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DessertDetailView.swift
+//  Fetch-Coding-Challenge
+//
+//  Created by Bo Zhong on 5/26/24.
+//
+
+import SwiftUI
+
+struct DessertDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DessertDetailView()
+}
